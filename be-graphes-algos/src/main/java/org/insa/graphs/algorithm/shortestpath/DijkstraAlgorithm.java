70f2c92030020cc92d241e699805c95b5581ace0
Paul Gervais <p.gervais17@laposte.net>
2021-04-12 12:19:30 +0200

    Dijkstra modif (non abouti)

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,9 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import java.util.HashMap;
+import org.insa.graphs.model.Node;
+
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -10,10 +14,68 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
-        BinaryHeap <Label> labels = new BinaryHeap <Label>();
+        List <Label> labels = new ArrayList
+        BinaryHeap <Label> tas = new BinaryHeap <Label>();
         HashMap <Node, Label> hashes = new HashMap <Node, Label> ();
         
+        labels.insert(new Label(data.getOrigin()));
         
+        
+        graph g = data.getGraph();
+        
+        for (Node node : g.getNodes()) {
+        	tas.put(node, new Label(node));
+        }
+        Label pluspetit = null;
+        
+        
+        node dest = data.getDestination();
+        while(!tas.isEmpty) {
+        	
+        
+        	pluspetit.setMarque();
+        	notifyNodeMarked(pluspetit.sommet_courant);
+        	
+            for (Arc arc : pluspetit.getCost().getSuccessors()) {
+
+                // On test si le chemin n'est pas permis, on skip
+                if (!data.isAllowed(arc)) continue;
+
+                Node node = arc.getDestination();
+                Label l = hashes.get(node);
+
+                // S'il est déjà marqué, on skip
+                if (l.marque) continue;
+
+                double n_cout = pluspetit.getCost() + data.getCost(arc);
+
+                if (l.cout > n_cout) {
+                    tas.tryremove(l);
+                    l.cout = n_cout;
+                    l.pere = pluspetit.sommet_courant;
+                    l.sommet_courant = node;
+                    notifyNodeReached(node);
+                    tas.insert(l);
+                }
+            }
+        }
+        
+        /* while (sommet not(GetMark) {
+            *     x=min(tas);
+            *     x.SetMark(true);
+            *     for(successors) {
+            *      if !(y.getMark()){
+                *     if (y.getcost() > x.getcost() + coutArc(x,y)) {
+                *         cout (y) = cout(x) + coutArc(x,y);
+                *         if exist(y,tas) {
+                *             update(y,tas)
+                *         }else{
+                *                 insert(y,tas);
+                *         }
+            *        }
+            *     }
+            *    }
+            *}*/
         // TODO:
         return solution;
     }
--- be-graphes-model/src/main/java/org/insa/graphs/model/Label.java
+++ be-graphes-model/src/main/java/org/insa/graphs/model/Label.java
@@ -1,20 +1,41 @@
 package org.insa.graphs.model;
 
-public class Label {
+public class Label implements Comparable <Label> {
 	
 	protected Node sommet_courant;
 	protected boolean marque;
-	protected int cout;
+	protected double cout;
 	private Arc pere;
+	
+	@Override
+	public int compareTo(Label other) {
+		return Double.compare(this.cout, other.cout);
+	}
 	
 	public Label (Node sommet) {
 		this.sommet_courant= sommet;
-		this.cout=1/0;
+		this.cout=1.0/0.0;
 		this.pere=null;
 	}
 	
-	private int getCost() {
+	private double getCost() {
 		return this.cout;
 	}
-
+	
+	private boolean getMarque() {
+		return this.marque;
+	}
+	
+	private void setMarque(boolean marque){
+		this.marque=marque;
+	}
+	
+	private void setArc (Arc pere) {
+		this.pere=pere;
+	}
+	
+	private void setCout (double cout) {
+		this.cout=cout;
+	}
+	
 }
